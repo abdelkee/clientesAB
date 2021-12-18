6be978a91adc505ae1bbc0f78e988cef18ff3362
abdelkee <abdell.anejjar@gmail.com>
2021-12-18 02:30:13 -0500

    add data to single client

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import TopBar from "../components/TopBar"
 export const clientsRoute = "https://clientes-ab.vercel.app/";
 // export const clientsRoute = "http://localhost:3000/";
 
-
 function Home({clientes}) {
   return (
     <div>
