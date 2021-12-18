409d59b54a73b052847c3048b24f78df0a5040c0
abdelkee <abdell.anejjar@gmail.com>
2021-12-18 02:04:56 -0500

    add staticprops

--- components/ClientCard.jsx
+++ components/ClientCard.jsx
@@ -1,23 +1,23 @@
 import Link from 'next/link';
 import Avatar from 'react-nice-avatar';
 
-function ClientCard() {
+function ClientCard({cliente}) {
     return (
-        // <Link href={'/'}>
-        //     <a>
-        //         <div className="card-container">
-        //             <Avatar style={{width: '75%', height: '50%'}} className="card-image" sex='woman' hairStyle='womanLong' isGradient={true}/>
-        //             <h4>{cliente.name}</h4>
-        //             <h3>$ 432.00</h3>
-        //         </div>
-        //     </a>
-        // </Link>
+        <Link href={'/'}>
+            <a>
+                <div className="card-container">
+                    <Avatar style={{width: '75%', height: '50%'}} className="card-image" sex='woman' hairStyle='womanLong' isGradient={true}/>
+                    <h4>{cliente.name}</h4>
+                    <h3>$ 432.00</h3>
+                </div>
+            </a>
+        </Link>
 
-        <div className="card-container">
-            <Avatar style={{width: '75%', height: '50%'}} className="card-image" sex='woman' hairStyle='womanLong' isGradient={true}/>
-            {/* <h4>{cliente.name}</h4> */}
-            <h3>$ 432.00</h3>
-        </div> 
+        // <div className="card-container">
+        //     <Avatar style={{width: '75%', height: '50%'}} className="card-image" sex='woman' hairStyle='womanLong' isGradient={true}/>
+        //     {/* <h4>{cliente.name}</h4> */}
+        //     <h3>$ 432.00</h3>
+        // </div> 
             
                 
             
--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,28 @@ import AddBtn from "../components/AddBtn"
 import Grid from "../components/Grid"
 import TopBar from "../components/TopBar"
 
-export const clientsRoute = process.env.NODE_ENV === "development" ? 'http://localhost:3000' : `https://${process.env.VERCEL_URL}`;
+// export const clientsRoute = "https://clientes-ab.vercel.app/";
+export const clientsRoute = "http://localhost:3000/";
 
 
-function Home() {
+function Home({clientes}) {
   return (
     <div>
       <TopBar name={''} amount={1.00}/>
-      {/* <Grid clientes={clientes}/> */}
+      <Grid clientes={clientes}/>
       <AddBtn type={'client'}/>
     </div>
   )
 }
 
-// export const getStaticProps = async() => {
+export const getStaticProps = async() => {
 
-//   const res = await fetch(clientsRoute + '/api/clients');
-//   const data = await res.json();
+  const res = await fetch(clientsRoute + 'api/clients');
+  const data = await res.json();
 
-//   return {
-//     props: {clientes: data}
-//   }
-// }
+  return {
+    props: {clientes: data}
+  }
+}
 
 export default Home
